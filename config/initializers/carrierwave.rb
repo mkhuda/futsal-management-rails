5ecce44c46965bfa7fb02fb0e534895071228c01
mkhuda <mhmmd.huda@gmail.com>
2016-02-17 20:53:03 +0700

    change to sftp

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,20 @@
 
 CarrierWave.configure do |config|
-  config.ftp_host = "45.32.252.124"
-  config.ftp_port = 21
-  config.ftp_user = "mkhuda"
-  config.ftp_passwd = "muhammad"
-  config.ftp_folder = "/var/www/html/sites/androgan/cdn/images"
-  config.ftp_url = "http://androgan.com/cdn/images"
-  config.ftp_passive = false # false by default
+  # config.ftp_host = "45.32.252.124"
+  # config.ftp_port = 21
+  # config.ftp_user = "mkhuda"
+  # config.ftp_passwd = "muhammad"
+  # config.ftp_folder = "/var/www/html/sites/androgan/cdn/images"
+  # config.ftp_url = "http://androgan.com/cdn/images"
+  # config.ftp_passive = false 
+
+  config.sftp_host = "45.32.252.124"
+  config.sftp_user = "mkhuda"
+  config.sftp_folder = "/var/www/html/sites/androgan/cdn/images"
+  config.sftp_url = "http://androgan.com/cdn/images"
+  config.sftp_options = {
+    :password => "muhammad",
+    :port     => 22
+  }
 
 end
