fff59bff2bfad283772a020cb3ad7b4d9aeeb84d
mkhuda <mhmmd.huda@gmail.com>
2016-02-22 00:20:16 +0700

    testimoni admin

---
+++ app/controllers/dashboard/testimonials_controller.rb
@@ -0,0 +1,22 @@
+class Dashboard::TestimonialsController < ApplicationController
+	before_filter :require_authorization
+
+	add_breadcrumb "Dashboard", :dashboard_path
+
+	def index
+		@fp = FutsalPlace.find_by(id: params[:futsal_place_id])
+	end
+
+	def destroy
+		@fp = FutsalPlace.find_by(id: params[:futsal_place_id])
+		@t = Testimonial.find(params[:id])
+		@t.destroy
+		redirect_to dashboard_futsal_place_testimonials_path
+	
+	end
+
+
+	def require_authorization
+		redirect_to root_path unless ((logged_in?[0]) && (logged_in?[1] == 'super'))
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
 
   namespace :dashboard do
     resources :futsal_places do
-      resources :prices, :bookings, :galleries
+      resources :prices, :bookings, :galleries, :testimonials
     end
     resources :users
   end
