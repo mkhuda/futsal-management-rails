c031e38b0ec80a223e1d9829abc6c47ebf707441
mkhuda <mhmmd.huda@gmail.com>
2017-03-28 15:26:43 +0700

    oke juga

--- app/assets/javascripts/reservation.js
+++ app/assets/javascripts/reservation.js
@@ -9,7 +9,6 @@ rs = function() {
 		var data_p = $(this).parents().eq(2).attr('data-p');
 		var gsub_id = the_id.replace(/\W/g, '');
 		$("#"+gsub_id+" .detail-reservation-box .the-btn-reservation").html(btn_success);
-		// alert(window.location.href);
 		$.ajax({
             type: "POST",
             url: data_p,
