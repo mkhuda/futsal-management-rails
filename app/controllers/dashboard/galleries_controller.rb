ca0e3b0f4c89904135bd84b022aeb01879ed7a91
mkhuda <mhmmd.huda@gmail.com>
2017-04-19 15:47:37 +0700

    beautify

--- app/controllers/dashboard/galleries_controller.rb
+++ app/controllers/dashboard/galleries_controller.rb
@@ -54,11 +54,11 @@ class Dashboard::GalleriesController < ApplicationController
 			params.require(:gallery).permit(:image)
 		end
 
-	def require_authorization
-		redirect_to root_path unless ((logged_in?[0]) && (logged_in?[1] == 'super'))
-	end
+    def require_authorization
+      redirect_to root_path unless ((logged_in?[0]) && (logged_in?[1] == 'super'))
+    end
 
-	def set_locale
-	  I18n.locale = params[:locale] || I18n.default_locale
-	end
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
 end
