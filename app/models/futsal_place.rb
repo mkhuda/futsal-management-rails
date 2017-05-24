5f9d7e2d4ea5b1a1d40d9adf1a8a3792ea52d3a3
mkhuda <mhmmd.huda@gmail.com>
2017-05-24 15:52:04 +0700

    tap

--- app/models/futsal_place.rb
+++ app/models/futsal_place.rb
@@ -7,7 +7,6 @@ class FutsalPlace < ActiveRecord::Base
 	has_many :galleries
 	has_many :testimonials
 	has_many :reservations
-	#     joins(:bookings).
 	#     group("bookings.id").
 	#     order("booking_count DESC").
 	#     limit(3)
