630d69c0f62252f9f32d19c614c30fea476cc3bd
mkhuda <mhmmd.huda@gmail.com>
2017-05-24 15:52:21 +0700

    tap

--- app/models/futsal_place.rb
+++ app/models/futsal_place.rb
@@ -7,7 +7,6 @@ class FutsalPlace < ActiveRecord::Base
 	has_many :galleries
 	has_many :testimonials
 	has_many :reservations
-	#     order("booking_count DESC").
 	#     limit(3)
 	def self.search(query)
 		where("name LIKE ? OR deskripsi LIKE ?", "%#{query}%", "%#{query}%")
